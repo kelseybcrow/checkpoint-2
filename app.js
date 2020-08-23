8ce618508e95b31b92505647e1bbd55fe8c57ec4
kelseybcrow <kelseybcrow@gmail.com>
2020-08-22 21:20:21 -0600

    progress

---
+++ app.js
@@ -0,0 +1,51 @@
+let sushi = 0;
+
+// let sushiCount;
+// let knifeCount;
+// let bambooMatCount;
+// let chefCount;
+// let chefHelperCount;
+
+let clickUpgrades = {
+    knives: {
+        price: 100,
+        quantity: 0,
+        multiplier: 2
+    },
+    bambooMat: {
+        price: 200,
+        quantity: 0,
+        multiplier: 3
+    }
+
+}
+
+let autoUpgrades = {
+    chef: {
+        price: 500,
+        quantity: 0,
+        multiplier: 20
+    },
+    chefHelper: {
+        price: 1000,
+        quantity: 0,
+        multiplier: 40
+    }
+}
+
+function makeSushi() {
+
+}
+
+function updateInventory() {
+
+}
+
+function gameUpgrades() {
+
+}
+
+function startInterval() {
+
+}
+
